181f3d0ac65684184f64929dac748b190ac5e4b4
vahidtvj <vahid.tvj@gmail.com>
2024-04-20 16:30:21 +0330

    dev: storybook color switch

--- .storybook/withGlobals.tsx
+++ .storybook/withGlobals.tsx
@@ -1,12 +1,17 @@
 import { StoryContext, StoryFn } from "@storybook/react"
 import React from "react"
 import { useEffect } from "react"
+import { useConfigState } from "../src/stores/configStore"
 import { useUIStore } from "../src/stores/uiStore"
 
 export const withGlobals = (Story: StoryFn, context: StoryContext) => {
-	const { locale, colorScheme } = context.globals
+	const { locale, colorScheme, colors } = context.globals
 	useEffect(() => {
 		useUIStore.setState({ colorScheme, lang: locale })
 	}, [locale, colorScheme])
+	useEffect(() => {
+		useConfigState.setState({ useMaterialYou: colors === "MaterialYou" })
+	}, [colors])
+
 	return <Story />
 }
