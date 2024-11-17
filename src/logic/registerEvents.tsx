2cb2b8ac4dfc999a7b181b0aac78bd61a2f9d7a6
vahidtvj <vahid.tvj@gmail.com>
2024-11-17 19:02:38 +0330

    fix notification action handler

--- src/logic/registerEvents.tsx
+++ src/logic/registerEvents.tsx
@@ -1,13 +1,12 @@
 import { changeDoseStatus } from "@/db"
 import { useDebugStore } from "@/stores/debugStore"
-import notifee, { EventType } from "@notifee/react-native"
+import notifee, { EventType, type Event } from "@notifee/react-native"
 import * as BackgroundFetch from "expo-background-fetch"
 import * as TaskManager from "expo-task-manager"
 import { onScheduleRunEvent } from "./dose"
 import { BACKGROUND_FETCH_TASK } from "./events"
 
-notifee.onForegroundEvent(async ({ type, detail }) => {
-	// ! handle notification actions
+async function handleNotificationEvent({ type, detail }: Event) {
 	if (
 		type === EventType.ACTION_PRESS &&
 		detail.pressAction?.id &&
@@ -17,7 +16,10 @@ notifee.onForegroundEvent(async ({ type, detail }) => {
 		const action = detail.pressAction.id as "skip" | "confirm"
 		await changeDoseStatus(id, action)
 	}
-})
+}
+
+notifee.onForegroundEvent(handleNotificationEvent)
+notifee.onBackgroundEvent(handleNotificationEvent)
 
 TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
 	useDebugStore.getState().push({ time: new Date(), type: "Service" })
