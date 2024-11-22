186132940fc395fa75ac822731ccf63a139a4a1e
vahidtvj <vahid.tvj@gmail.com>
2024-11-22 21:12:19 +0330

    fix onCascade not working

--- src/db/query/client.ts
+++ src/db/query/client.ts
@@ -3,4 +3,7 @@ import { openDatabaseSync } from "expo-sqlite/next"
 import * as schema from "../schema"
 
 export const expoDb = openDatabaseSync("db", { enableChangeListener: true })
+// once per connection
+expoDb.execAsync("PRAGMA foreign_keys = ON;")
+
 export const db = drizzle(expoDb, { schema })
