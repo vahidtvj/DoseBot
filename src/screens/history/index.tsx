d78e9ac6cc7d7031b0082ee9d517f24abe7f6cf4
vahidtvj <vahid.tvj@gmail.com>
2025-01-03 14:57:33 +0330

    feat: history tab summery

--- src/screens/history/index.tsx
+++ src/screens/history/index.tsx
@@ -1,13 +1,45 @@
+import { MedicineCard } from "@/components/cards/medicine"
+import { IconText } from "@/components/common/IconText"
+import { ItemPicker } from "@/components/pickers/itemPicker"
 import { HistoryCalendar } from "@/components/views/history/historyCalendar"
-import { type IDose, getDoseHistory } from "@/db"
+import { MedIconMap } from "@/constants"
+import {
+	type IDose,
+	getAllMeds,
+	getDoseHistory,
+	getDoseSummery,
+	getMed,
+} from "@/db"
 import type { HomeTabScreenProps } from "@/routes/types"
+import { useAppTheme } from "@/theme"
 import { useFocusEffect } from "@react-navigation/native"
+import { addDays } from "date-fns"
+import { useLiveQuery } from "drizzle-orm/expo-sqlite"
 import { useCallback, useRef, useState } from "react"
-import { StyleSheet, View } from "react-native"
+import { useTranslation } from "react-i18next"
+import { FlatList, StyleSheet, View } from "react-native"
+import { Button, Chip, Text } from "react-native-paper"
 
 export default function Page({ navigation }: HomeTabScreenProps<"History">) {
+	const [filterOpen, setFilterOpen] = useState(false)
+	const [filter, setFilter] = useState<number>(0)
+	const [pageMode, setPageMode] = useState<"summary" | "calendar">("calendar")
+	const [summery, setSummery] = useState<
+		{
+			medId: number
+			data: {
+				range: "lastMonth" | "last6Month" | "all"
+				confirmed: number
+				total: number
+			}[]
+		}[]
+	>([])
+
 	const window = useRef<{ [key: number]: { start: Date; end: Date } }>([])
 	const [data, setData] = useState<{ [key: number]: IDose[] }>([])
+
+	const { data: filterMed } = useLiveQuery(getMed(filter), [filter])
+	const { data: allMeds } = useLiveQuery(getAllMeds)
 
 	const getNewData = useCallback(
 		async (props: {
@@ -20,31 +52,145 @@ export default function Page({ navigation }: HomeTabScreenProps<"History">) {
 				await Promise.all(
 					Object.entries(props).map(async ([k, v]) => [
 						Number(k),
-						await getDoseHistory(v),
+						await getDoseHistory({
+							...v,
+							medId: filter === 0 ? undefined : filter,
+						}),
 					]),
 				),
 			)
 			window.current = props
 			setData(newData)
 		},
-		[],
+		[filter],
 	)
 
 	useFocusEffect(
 		useCallback(() => {
-			if (Object.keys(window.current).length === 0) return
-			getNewData(window.current)
-		}, [getNewData]),
+			if (pageMode === "summary") {
+				const now = new Date()
+				const dateRanges = {
+					lastMonth: { start: addDays(now, -30), end: now },
+					last6Month: { start: addDays(now, -180), end: now },
+					all: { start: null, end: null },
+				}
+				getDoseSummery({ dateRanges }).then((data) => setSummery(data))
+			} else if (pageMode === "calendar") {
+				if (Object.keys(window.current).length !== 0) getNewData(window.current)
+			}
+		}, [getNewData, pageMode]),
 	)
+
+	const { t } = useTranslation()
+
+	const onFilterSelect = useCallback((value?: number) => {
+		value !== undefined && setFilter(value)
+		setFilterOpen(false)
+	}, [])
 
 	return (
 		<View style={styles.page}>
-			<HistoryCalendar
-				data={data}
-				getNewData={getNewData}
-				onSelect={(date) =>
-					navigation.navigate("HistoryDay", { dayTimestamp: date.getTime() })
-				}
+			{pageMode === "calendar" ? (
+				<View style={styles.page}>
+					<View style={{}}>
+						<View
+							style={{
+								padding: 12,
+								paddingHorizontal: 48,
+								flexDirection: "row",
+								justifyContent: "space-between",
+								alignItems: "center",
+							}}
+						>
+							<IconText variant="bodyLarge" icon="filter">
+								{`${t("filter")}:`}
+							</IconText>
+							<Chip
+								icon={
+									filter && filterMed?.type
+										? MedIconMap[filterMed.type]
+										: "filter"
+								}
+								onPress={() => setFilterOpen(true)}
+							>
+								{filter ? filterMed?.name : t("allMeds")}
+							</Chip>
+						</View>
+					</View>
+					<HistoryCalendar
+						data={data}
+						getNewData={getNewData}
+						onSelect={(date) =>
+							navigation.navigate("HistoryDay", {
+								dayTimestamp: date.getTime(),
+							})
+						}
+					/>
+				</View>
+			) : (
+				<View style={styles.page}>
+					<FlatList
+						data={allMeds}
+						keyExtractor={(x) => String(x.id)}
+						renderItem={({ item }) => (
+							<MedicineCard
+								{...item}
+								schedules={[]}
+								inventoryEnabled={false}
+								extraData={
+									<View style={{ alignSelf: "flex-start" }}>
+										{summery
+											.find((x) => x.medId === item.id)
+											?.data.map((sum) => (
+												<View
+													style={{
+														flexDirection: "row",
+														justifyContent: "space-between",
+														gap: 12,
+													}}
+													key={sum.range}
+												>
+													<Text variant="labelLarge">{`${t(
+														`medHistory.${sum.range}`,
+													)} :`}</Text>
+													<View style={{ flexDirection: "row", gap: 6 }}>
+														<Text>{`${sum.confirmed}/${sum.total}`}</Text>
+														<Text style={{}}>{`${
+															Math.floor((100 * sum.confirmed) / sum.total) || 0
+														} %`}</Text>
+													</View>
+												</View>
+											))}
+									</View>
+								}
+							/>
+						)}
+					/>
+				</View>
+			)}
+			<View style={{ padding: 12, alignItems: "center", width: "100%" }}>
+				<Button
+					mode="contained"
+					style={{ maxWidth: 340, width: "100%" }}
+					onPress={() =>
+						setPageMode(pageMode === "calendar" ? "summary" : "calendar")
+					}
+				>
+					{pageMode === "calendar" ? t("summary") : t("calendar")}
+				</Button>
+			</View>
+			<ItemPicker
+				open={filterOpen}
+				onSelect={onFilterSelect}
+				selected={filter}
+				values={[
+					{ key: 0, label: "All Medications" },
+					...allMeds.map((x) => ({
+						key: x.id,
+						label: x.name,
+						icon: MedIconMap[x.type],
+					})),
+				]}
 			/>
 		</View>
 	)
