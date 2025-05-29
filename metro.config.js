a5e08894ba3493a08e5bc017ac01362b7cf1911a
vahidtvj <vahid.tvj@gmail.com>
2025-05-29 14:06:03 +0330

    remove sentry

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,6 @@
 // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
 const path = require("path")
 const { getDefaultConfig } = require("expo/metro-config")
-
 const withStorybook = require("@storybook/react-native/metro/withStorybook")
 
 /** @type {import('expo/metro-config').MetroConfig} */
